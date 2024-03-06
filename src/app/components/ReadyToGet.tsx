73f6003c4b1aeebe0dcf66f491fe4406ef45c490
tylerb1 <tberb1290@gmail.com>
2024-03-05 21:21:37 -0600

    final style fixes for Ready section

--- src/app/components/ReadyToGet.tsx
+++ src/app/components/ReadyToGet.tsx
@@ -16,25 +16,27 @@ const readySteps = [
 
 export default function ReadyToGet() {
   return (
-    <div className="ready-to-get-m3-container bg-white px-8 py-16 w-full flex flex-col items-start lg:items-center">
-      <div className="ready-info-container flex flex-col gap-4 mb-6 lg:gap-8 lg:flex-row">
-        <Image className="object-fit" src="/images/other/JaTawn-1270.png" alt="ready-to-get-image" width={587} height={429} />
-        <div className="ready-steps-container max-w-96 flex flex-col gap-4">
-          <div className="ready-title">
-            <p className={`text-primary-900 text-xl ml-6 lg:ml-0 ${sangbleuKingdomLight.className}`}>Ready to get</p>
-            <p className={`text-primary-900 text-3xl lg:text-5xl lg:ml-12 ${sangbleuEmpireBlackItalic.className}`}>M3?</p>
+    <div className="ready-to-get-m3-container bg-white px-8 py-16 w-full flex flex-col items-center">
+      <div className="ready-to-get-m3-content flex flex-col items-center gap-8 lg:gap-12">
+        <div className="ready-info-container flex flex-col gap-4 lg:gap-16 lg:flex-row items-center">
+          <Image className="object-fit" src="/images/other/JaTawn-1270.png" alt="ready-to-get-image" width={587} height={429} />
+          <div className="ready-steps-container max-w-[500px] flex flex-col gap-6">
+            <div className="ready-title">
+              <p className={`text-primary-900 text-2xl ml-6 lg:ml-0 ${sangbleuKingdomLight.className}`}>Ready to get</p>
+              <p className={`text-primary-900 text-5xl lg:text-5xl lg:ml-12 ${sangbleuEmpireBlackItalic.className}`}>M3?</p>
+            </div>
+            {readySteps.map((step, index) => {
+              return (
+                <div className="flex flex-row items-center" key={index}>
+                  <p className={`text-spacegray text-4xl min-w-12 ${sangbleuEmpireBlack.className}`}>{(index + 1) + '.'}</p>
+                  <p className={`text-spacegray text-xl ${sangbleuKingdomRegular.className}`} key={index}>{step}</p>
+                </div>
+              )
+            })}
           </div>
-          {readySteps.map((step, index) => {
-            return (
-              <div className="flex flex-row items-center" key={index}>
-                <p className={`text-spacegray text-4xl min-w-12 ${sangbleuEmpireBlack.className}`}>{(index + 1) + '.'}</p>
-                <p className={`text-spacegray ${sangbleuKingdomRegular.className}`} key={index}>{step}</p>
-              </div>
-            )
-          })}
         </div>
+        <GetM3Button text="GET YOUR M3" />
       </div>
-      <GetM3Button text="GET YOUR M3" />
     </div>
   );
 }
